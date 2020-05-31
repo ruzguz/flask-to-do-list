63881dbcb08342f02645d7eeac57b1a7731a6960
Rusben Guzman <rusbenguzman@gmail.com>
2020-05-30 20:14:42 -0400

    Create index route

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    # Getting user IP and save it in a cookie
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+
+@app.route('/hello')
 def hello():
-    return('Hello world from Flask!!!')
+    # Getting user IP
+    user_ip = request.cookies.get('user_ip')
+    
+    return('Hello world from Flask!!! - Your IP is {}'.format(user_ip))
