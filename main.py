bef03e40b08837e35a6117db3184c21d38003439
Rusben Guzman <rusbenguzman@gmail.com>
2020-06-01 23:07:30 -0400

    Install flask-bootstrap and modify base.html and navbar templates

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 todos = [ 'TODO 1', 'TODO 2', 'TODO 3' ]
 
